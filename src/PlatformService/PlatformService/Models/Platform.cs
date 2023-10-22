30f869960844d9e30bf31f867ff32cbe9ad04d74
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-22 15:42:13 +0200

    Add platform model

---
+++ src/PlatformService/PlatformService/Models/Platform.cs
@@ -0,0 +1,20 @@
+﻿namespace PlatformService.Models;
+
+public class Platform
+{
+    public Platform(int id, string name, string publisher, string cost)
+    {
+        Id = id;
+        Name = name;
+        Publisher = publisher;
+        Cost = cost;
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string Cost { get; set; }
+}
