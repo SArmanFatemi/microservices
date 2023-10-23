077678e2d8d4bbcb3e33f0ed6e1e2ecdee86c057
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-23 18:41:16 +0200

    Implement data layers

---
+++ src/PlatformService/PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,14 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepository
+{
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+
+    Task<IEnumerable<Platform>> GetAllAsync(CancellationToken cancellationToken);
+
+    Task<Platform?> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task CreateAsync(Platform platform, CancellationToken cancellationToken);
+}
---
+++ src/PlatformService/PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using PlatformService.Persistence;
+
+namespace PlatformService.Data;
+
+public class PlatformRepository : IPlatformRepository
+{
+    private readonly DatabaseContext databaseContext;
+
+    public PlatformRepository(DatabaseContext databaseContext)
+    {
+        this.databaseContext = databaseContext;
+    }
+
+    public async Task CreateAsync(Platform platform, CancellationToken cancellationToken) =>
+        await databaseContext.Platforms.AddAsync(platform, cancellationToken);
+
+    public async Task<IEnumerable<Platform>> GetAllAsync(CancellationToken cancellationToken) =>
+        await databaseContext.Platforms.ToListAsync(cancellationToken);
+    
+
+    public async Task<Platform?> GetByIdAsync(int id, CancellationToken cancellationToken) =>
+        await databaseContext.Platforms.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+    
+
+    public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken) =>
+        await databaseContext.SaveChangesAsync(cancellationToken) >= 0;
+}
