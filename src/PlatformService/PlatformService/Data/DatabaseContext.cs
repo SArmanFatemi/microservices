4ac432bb78334b3d959424b071434b733c891531
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-23 18:14:18 +0200

    Add database context

---
+++ src/PlatformService/PlatformService/Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Platform> Platforms { get; set; }
+}
