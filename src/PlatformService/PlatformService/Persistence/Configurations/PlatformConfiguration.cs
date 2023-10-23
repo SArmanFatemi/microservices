a17ef82f5493cace82d1976f6605a9586f73b455
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-23 18:58:41 +0200

    Add seedin platform data

--- src/PlatformService/PlatformService/Persistence/Configurations/PlatformConfiguration.cs
+++ src/PlatformService/PlatformService/Persistence/Configurations/PlatformConfiguration.cs
@@ -14,6 +14,13 @@ namespace PlatformService.Persistence.Configurations
             builder.Property(e => e.Name).IsRequired();
             builder.Property(e => e.Cost).IsRequired();
             builder.Property(e => e.Publisher).IsRequired();
+
+            builder.HasData(new List<Platform>
+            {
+                new Platform(1, "dotnet", "Microsoft", "Free"),
+                new Platform(2, "SQL Server Express", "Microsoft", "Free"),
+                new Platform(3, "Kubernates", "CNCF", "Free"),
+            });
         }
     }
 }
