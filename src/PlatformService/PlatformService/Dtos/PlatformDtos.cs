d50047b717198d1bedca2bc5cb8ed84d07274e31
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-24 08:38:10 +0200

    Implement platform dtos

---
+++ src/PlatformService/PlatformService/Dtos/PlatformDtos.cs
@@ -0,0 +1,17 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Dtos;
+
+public record PlatformResponse(int Id, string Name, string Publisher, string Cost);
+
+// TODO: We should implement validation later
+public record PlatformRequest(string Name, string Publisher, string Cost);
+
+public static class PlatformMappers
+{
+    public static PlatformResponse ToResponse(this Platform platform) =>
+        new(platform.Id, platform.Name, platform.Publisher, platform.Cost);
+
+    public static Platform ToModel(this PlatformRequest request, int? id = null) =>
+        new(id ?? 0, request.Name, request.Publisher, request.Cost);
+}
