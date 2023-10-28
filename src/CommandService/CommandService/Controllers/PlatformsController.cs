e0167fd10b95b4cbd930ca5b25543228c2d8db60
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2023-10-28 13:39:04 +0200

    Implement platform controller with test incound action

---
+++ src/CommandService/CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers;
+
+
+[Route("api/commands/platforms")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult TestInbuondConnection()
+    {
+        Console.WriteLine("--> Inbound POST # Command Service");
+        return Ok("Inbound test from Platforms Controller");
+    }
+}
